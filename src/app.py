da8387bb409bb679f43c5510d58fc0b57842cf7e
Héctor Chocobar-Torrejón <hector@chocobar.net>
2023-01-14 20:50:38 +0000

    feature: GET Users People Planets

--- src/app.py
+++ src/app.py
@@ -8,8 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Characters, Planets
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -26,26 +26,111 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
+# Generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+# Endpoint /user
+@app.route('/user', methods=['GET', 'POST'])
+def user():
+    if request.method == 'GET':
+        users = User.query.all()
+        results = [user.serialize() for user in users]
+        response_body = {"message": "ok",
+                         "total_records": len(results),
+                         "results": results}
+        return response_body, 200
+        # return jsonify(response_body), 200
+    if request.method == 'POST':
+        request_body = request.get_json()
+        user = User(email = request_body['email'],
+                    password = request_body['password'],
+                    is_active = request_body['is_active'])
+        db.session.add(user)
+        db.session.commit()
+        return request_body, 200
+        # return jsonify(request_body), 200
 
-    return jsonify(response_body), 200
 
-# this only runs if `$ python src/app.py` is executed
+# Endpoint /user/<int:user_id>
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if user:
+        results = user.serialize()
+        response_body = {"message": "ok",
+                         "total_records": 1,
+                         "results": results}
+        return response_body, 200
+    else:
+        response_body = {"message": "record not found"}
+        return response_body, 200
+
+
+# Endpoint /people
+@app.route('/people', methods=['GET'])
+def people():
+    if request.method == 'GET':
+        characters = Characters.query.all()
+        results = [character.serialize() for character in characters]
+        response_body = {"message": "ok",
+                         "total_records": len(results),
+                         "results": results}
+        return response_body, 200
+
+
+# Endpoint /people/<int:people_id>
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_people(people_id):
+    character = Characters.query.filter_by(id=people_id).first()
+    if character:
+        results = character.serialize()
+        response_body = {"message": "ok",
+                         "total_records": 1,
+                         "results": results}
+        return response_body, 200
+    else:
+        response_body = {"message": "record not found"}
+        return response_body, 200
+
+
+# Endpoint /planets
+@app.route('/planets', methods=['GET'])
+def planet():
+    if request.method == 'GET':
+        planets = Planets.query.all()
+        results = [planet.serialize() for planet in planets]
+        response_body = {"message": "ok",
+                         "total_records": len(results),
+                         "results": results}
+        return response_body, 200
+
+
+# Endpoint /planets/<int:planet_id>
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planets.query.filter_by(id=planet_id).first()
+    if planet:
+        results = planet.serialize()
+        response_body = {"message": "ok",
+                         "total_records": 1,
+                         "results": results}
+        return response_body, 200
+    else:
+        response_body = {"message": "record not found"}
+        return response_body, 200
+
+
+# This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -12,8 +13,52 @@ class User(db.Model):
         return '<User %r>' % self.username
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+        return {"id": self.id,
+                "email": self.email,
+                "is_active": self.is_active}
+
+
+class Characters (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    gender = db.Column(db.String(50))
+    height = db.Column(db.Integer)
+    eyes_color = db.Column(db.String(50))
+    hair_color = db.Column(db.String(50))
+    skin_color = db.Column(db.String(50))
+
+    def __repr__(self):
+        return '<Characters %r>' % self.id
+
+    def serialize(self):
+        return {"id": self.id,
+                "name": self.name,
+                "gender": self.gender,
+                "height": self.height,
+                "eyes_color": self.eyes_color,
+                "hair_color": self.hair_color,
+                "skin_color": self.skin_color}
+
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    population = db.Column(db.String(250))
+    terrain = db.Column(db.String(250))
+    climate = db.Column(db.String(100))
+    orbital_period = db.Column(db.String(100))
+    rotation_period = db.Column(db.String(100))
+    diameter = db.Column(db.String(100))
+
+    def __repr__(self):
+        return '<Planets %r>' % self.id
+
+    def serialize(self):
+        return {"id": self.id,
+                "name": self.name,
+                "population": self.population,
+                "terrain": self.terrain,
+                "climate": self.climate,
+                "orbital_period": self.orbital_period,
+                "rotation_period": self.rotation_period,
+                "diameter": self.diameter}
